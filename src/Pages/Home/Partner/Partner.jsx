7d122d3d9366d339891ed719f454320dc335de60
hasiburrahmanakash79 <hasiburrahmanakash79@gmail.com>
2025-02-04 23:17:29 +0600

    change logo for partner company

--- src/Pages/Home/Partner/Partner.jsx
+++ src/Pages/Home/Partner/Partner.jsx
@@ -5,51 +5,7 @@ const Partner = () => {
         <div className="container mx-auto py-10 text-center">
             <p>Partners section</p>
             <div className="flex gap-10 flex-wrap">
-                <div className="w-12 mx-auto">
-                <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/8/82/Dell_Logo.png/1598px-Dell_Logo.png?20160918115731" alt="partner1" />
-                </div>
-                <div className="w-12 mx-auto">
-                <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/8/82/Dell_Logo.png/1598px-Dell_Logo.png?20160918115731" alt="partner1" />
-                </div>
-                <div className="w-12 mx-auto">
-                <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/8/82/Dell_Logo.png/1598px-Dell_Logo.png?20160918115731" alt="partner1" />
-                </div>
-                <div className="w-12 mx-auto">
-                <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/8/82/Dell_Logo.png/1598px-Dell_Logo.png?20160918115731" alt="partner1" />
-                </div>
-                <div className="w-12 mx-auto">
-                <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/8/82/Dell_Logo.png/1598px-Dell_Logo.png?20160918115731" alt="partner1" />
-                </div>
-                <div className="w-12 mx-auto">
-                <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/8/82/Dell_Logo.png/1598px-Dell_Logo.png?20160918115731" alt="partner1" />
-                </div>
-                <div className="w-12 mx-auto">
-                <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/8/82/Dell_Logo.png/1598px-Dell_Logo.png?20160918115731" alt="partner1" />
-                </div>
-                <div className="w-12 mx-auto">
-                <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/8/82/Dell_Logo.png/1598px-Dell_Logo.png?20160918115731" alt="partner1" />
-                </div>
-                <div className="w-12 mx-auto">
-                <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/8/82/Dell_Logo.png/1598px-Dell_Logo.png?20160918115731" alt="partner1" />
-                </div>
-                <div className="w-12 mx-auto">
-                <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/8/82/Dell_Logo.png/1598px-Dell_Logo.png?20160918115731" alt="partner1" />
-                </div>
-                <div className="w-12 mx-auto">
-                <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/8/82/Dell_Logo.png/1598px-Dell_Logo.png?20160918115731" alt="partner1" />
-                </div>
-                <div className="w-12 mx-auto">
-                <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/8/82/Dell_Logo.png/1598px-Dell_Logo.png?20160918115731" alt="partner1" />
-                </div>
-                <div className="w-12 mx-auto">
-                <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/8/82/Dell_Logo.png/1598px-Dell_Logo.png?20160918115731" alt="partner1" />
-                </div>
-                <div className="w-12 mx-auto">
-                <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/8/82/Dell_Logo.png/1598px-Dell_Logo.png?20160918115731" alt="partner1" />
-                </div>
-                <div className="w-12 mx-auto">
-                <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/8/82/Dell_Logo.png/1598px-Dell_Logo.png?20160918115731" alt="partner1" />
-                </div>
+                
             </div>
         </div>
     );
